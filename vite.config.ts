d022cfa1081bb5796eeceab8081725b68f2d8fd1
Yauhen Kviatkouski <kvetkovski@gmail.com>
2024-03-20 21:06:24 +0100

    chore: add scss options

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import manifestGenerator from "./scripts/manifestGenerator";
+import { defineConfig } from "vite";
 import packageJson from "./package.json";
+import manifestGenerator from "./scripts/manifestGenerator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [manifestGenerator(), preact()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        includePaths: ["./src/styles"],
+      },
+    },
+  },
   build: {
     rollupOptions: {
       output: {
